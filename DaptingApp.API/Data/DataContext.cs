2f29f2d0a22f9c2830ebbe00e9481751c9bb5c28
Arvind Tulsiram <arvind_t@msn.com>
2019-08-07 13:18:24 +0530

    Initial Commit

---
+++ DaptingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DaptingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaptingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}        
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DaptingApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DaptingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
